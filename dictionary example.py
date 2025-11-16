ddf78843885dbd56af37ad385de85abd3bf11600
Mekidddd <2404030018@students.unis.ac.id>
2025-11-16 19:17:20 +0700

    Add files via upload

---
+++ dictionary example.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Arif",
+    "age": 21,
+    "major": "Informatics"
+}
+
+print("Dictionary:", student)
+
+print("Name:", student["name"])
+
+student["GPA"] = 3.8
+print("After add:", student)
